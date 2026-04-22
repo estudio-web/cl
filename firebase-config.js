b4d1a2590e63d147b99d14143200a2fa69f94764
estudio-web <tompsonfulner@gmail.com>
2026-04-21 23:38:46 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCTp21DISEc-HtIV4SVNZqnxX8q48o7r_A",
+  authDomain: "comercio-5cd9b.firebaseapp.com",
+  projectId: "comercio-5cd9b",
+  storageBucket: "comercio-5cd9b.firebasestorage.app",
+  messagingSenderId: "400441545760",
+  appId: "1:400441545760:web:368ede6547e0ccee055845"
+};
+
+// API KEY de ImgBB
+export const IMGBB_API_KEY = "1de3e00f4acb42305d5b8621ce7deebb";
+
+// Config general
+export const APP_CONFIG = {
+  collectionName: "products",
+  currencySymbol: "$",
+  whatsappNumber: "5490000000000" // opcional, luego lo cambiás
+};
